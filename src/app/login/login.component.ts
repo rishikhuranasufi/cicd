57554f22dc6619836a2115eee191952f44915f9f
Rishi Khurana <Rishi.Khurana@team.telstra.com>
2018-03-30 12:16:42 +0530

    Login page changes

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
+  myvar = false;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  login(){
+    console.log('IN LOGIN');
+    this.myvar = true;
   }
 
 }
